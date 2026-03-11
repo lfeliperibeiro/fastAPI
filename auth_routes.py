47d5ef2532004ba9d02334cbf8354bef33845ebc
Felipe Ribeiro <piporibeiro@gmail.com>
2026-03-10 21:26:00 -0300

    feat: implement authentication routes and session management

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import User
+from dependencies import get_session
+from main import bcrypt_context
+from schemas import userSchema
+from sqlalchemy.orm import Session
 
 auth_router  = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -7,3 +12,17 @@ auth_router  = APIRouter(prefix="/auth", tags=["auth"])
 async def auth():
     """Authentication routes"""
     return {"message": "Authentication route"}
+
+@auth_router.post("/signup")
+
+async def signup(user_schema: userSchema, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.email == user_schema.email).first()
+
+    if user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    else:
+        password = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.name, user_schema.email, password, user_schema.admin, user_schema.active)
+        session.add(new_user)
+        session.commit()
+        return {"message": f"User created successfully {user_schema.email}"}
